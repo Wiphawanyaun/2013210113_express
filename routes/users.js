e54b8909158bb0d608c194a734c85fb90de5e3b6
Wiphawan Piapramong <gitwiphawan@gmail.com>
2022-11-26 11:20:32 +0700

    ws_A

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('Hello kitty')
+  res.status(200).json({
+    fullname:'Wiphawan Piapramong'
+  })
 });
+
+router.get('/bio', function(req, res, next) {
+  // res.send('Hello kitty')
+  res.status(200).json({
+    fullname:'Wiphawan Piapramong',
+    nickname : 'Yaun',
+    hobby:'playing game',
+    gitusername:'Wiphawanyaun'
+  })
+});
+
 
 module.exports = router;
