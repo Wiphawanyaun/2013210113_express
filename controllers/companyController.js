33e1f61e170cde9602caa68002291bb771fa85dd
Wiphawan Piapramong <gitwiphawan@gmail.com>
2022-12-10 11:02:14 +0700

    connect_DB

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -1,32 +1,36 @@
 const data = [
-    {
-      id: 1,
-      name: "Studio Ghibli",
-      address: {
-        province: "Japan",
-        postcode: "880-8501",
-      },
+  {
+    id: 1,
+    name: "Studio Ghibli",
+    address: {
+      province: "Japan",
+      postcode: "880-8501",
     },
-    {
-      id: 2,
-      name: "Capcom",
-      address: {
-        province: "Japan",
-        postcode: "540-0037",
-      },
+  },
+  {
+    id: 2,
+    name: "Capcom",
+    address: {
+      province: "Japan",
+      postcode: "540-0037",
     },
-    {
-      id: 3,
-      name: "Square Enix",
-      address: {
-        province: "Japan",
-        postcode: "160-8430",
-      },
+  },
+  {
+    id: 3,
+    name: "Square Enix",
+    address: {
+      province: "Japan",
+      postcode: "160-8430",
     },
-  ];
-  
-  exports.company = (req, res, next) => {
-    res.status(200).json({
-      data: data,
-    });
-  };
+  },
+];
+
+const Company = require("../models/company");
+
+exports.index = async (req, res, next) => {
+  const company = await Company.findOne();
+
+  res.status(200).json({
+    data: company,
+  });
+};
--- routes/company.js
+++ routes/company.js
@@ -2,6 +2,6 @@ var express = require("express");
 var router = express.Router();
 const companyController = require("../controllers/companyController");
 
-router.get("/", userController.company);
+router.get("/", companyController.index);
 
 module.exports = router;
