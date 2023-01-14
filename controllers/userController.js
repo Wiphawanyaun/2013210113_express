80e369182c90fbf77e25c92834398f1ec477015f
Wiphawan Piapramong <gitwiphawan@gmail.com>
2023-01-14 10:22:46 +0700

    hashPassword _success

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,6 @@
+const User = require("../models/user");
+
+
 exports.index = (req, res, next) => {
   res.status(200).json({
     fullname: "Wiphawan Piapramong",
@@ -12,3 +15,18 @@ exports.bio = (req, res, next) => {
     gitusername: "Wiphawanyaun",
   });
 };
+
+
+exports.register = async(req,res,next) =>{
+  const{name,email,password} = req.body;
+
+  let user = new User();
+  user.name = name
+  user.email = email
+  user.password= await user.encryptPassword(password)
+
+  await user.save()
+  res.status(201).json({
+    message:'ลงทะเบียนเรียบร้อยแล้ว'
+  });
+}
